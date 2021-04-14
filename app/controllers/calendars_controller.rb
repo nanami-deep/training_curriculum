6afb073bf5491742b9d065497de895f384721bc4
nanami-deep <79752026+nanami-deep@users.noreply.github.com>
2021-04-14 17:17:16 +0900

    Revert "Issue1~3の修正"
    This reverts commit bc34f20cfeea016d3da0a9915da2a9843e570119.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,20 +1,20 @@
 class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
-  def index
-    get_week
+  def get_index
+    getWeek
     @plan = Plan.new
   end
 
   # 予定の保存
-  def create
+  def get_create
     Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
-  def plan_params
+  def get_params
     params.require(:calendars).permit(:date, :plan)
   end
 
